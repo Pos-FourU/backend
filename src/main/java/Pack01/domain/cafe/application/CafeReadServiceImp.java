049388fb355a5089f22d3ec6a1c7d614e417df47
장원진 <wkd3828@gmail.com>
2023-06-16 11:18:17 +0900

    interface추가

---
+++ src/main/java/Pack01/domain/cafe/application/CafeReadServiceImp.java
@@ -0,0 +1,33 @@
+package Pack01.domain.cafe.application;
+
+import Pack01.domain.cafe.dto.CafeFindRespDto;
+import Pack01.domain.cafe.entity.Cafe;
+import Pack01.domain.cafe.repository.CafeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CafeReadServiceImp implements CafeReadService {
+
+    private final CafeRepository cafeRepository;
+    public List<CafeFindRespDto> findAll() {
+        List<Cafe> all = cafeRepository.findAll();
+        return all
+                .stream()
+                .map(cafe -> new CafeFindRespDto()
+                        .of(cafe.getCafe_id(),
+                        cafe.getMember_id(),
+                        cafe.getCafe_name(),
+                        cafe.getCafe_address(),
+                        cafe.getCafe_latitude(),
+                        cafe.getCafe_longitude()))
+                .collect(Collectors.toList());
+    }
+
+}
--- src/main/java/Pack01/domain/member/application/MemberReadService.java
+++ src/main/java/Pack01/domain/member/application/MemberReadService.java
@@ -1,16 +1,4 @@
 package Pack01.domain.member.application;
 
-import Pack01.domain.member.dto.MemberRegisterReqDto;
-import Pack01.domain.member.entity.Member;
-import Pack01.domain.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class MemberReadService {
-
-
+public interface MemberReadService {
 }
