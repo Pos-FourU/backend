bf566e82a27751f6cfa4c9fa29c73af6f0dbeadc
yunseo <ya000123@naver.com>
2023-06-17 11:56:40 +0900

    예약하기에 이미 있는 사용자는 빼기 UI 추가

--- src/main/java/Pack01/domain/reservation/application/ReservationWriteService.java
+++ src/main/java/Pack01/domain/reservation/application/ReservationWriteService.java
@@ -4,4 +4,5 @@ import Pack01.domain.reservation.dto.ReservationRegistDto;
 
 public interface ReservationWriteService {
     void register(ReservationRegistDto reservationRegistDto);
+    boolean checkIfAlreadyReserved(Long memberId);
 }
