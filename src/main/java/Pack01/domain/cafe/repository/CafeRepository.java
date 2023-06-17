b1162b07b5ffd866b26a3f1052c3d5ccbcb87cdf
장원진 <wkd3828@gmail.com>
2023-06-17 10:04:34 +0900

    멤버

--- src/main/java/Pack01/domain/cafe/repository/CafeRepository.java
+++ src/main/java/Pack01/domain/cafe/repository/CafeRepository.java
@@ -50,6 +50,10 @@ public class CafeRepository {
                     .build();
         }
     }
+    public Integer existCafeByMemberId(Long member_id){
+        String sql = "SELECT COUNT(*) FROM "+TABLE+"  WHERE member_id = ?";
+        return jdbcTemplate.queryForObject(sql, Integer.class, member_id);
+    }
 
     public List<CafeLeftCountRespDto> getCafeLeftItemCount() {
         String sql = "SELECT a.cafe_id, (COALESCE(a.total, 0) - COALESCE(b.res, 0) - COALESCE(c.ren, 0)) AS totals,d.cafe_name,d.cafe_latitude, d.cafe_longitude\n" +
